0f7249eb4b23ab9c80b0f4ae4f4daf32df5de1f0
TusharKadam7671 <kadamtushar080@gmail.com>
2022-09-28 22:26:41 +0530

    DBUtil class created

---
+++ Automated_Auction_System/src/com/app/utility/DBUtil.java
@@ -0,0 +1,44 @@
+package com.app.utility;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBUtil {
+
+	public static Connection provideConnection()
+	{
+		
+		Connection conn = null;
+		
+		
+		
+		
+		try {
+			
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			
+		} catch (ClassNotFoundException e) {
+			
+			e.printStackTrace();
+		}
+		
+		
+		String url = "jdbc:mysql://localhost:3306/projectdb";
+		
+		try {
+			
+			conn = DriverManager.getConnection(url, "root", "groot");
+			
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+		
+		
+		
+		
+		return conn;
+		
+	}
+}
